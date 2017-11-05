5579be284aa9fc3a287a095710b1baf929a2f525
Christoph Winter <mail.christoph.winter@gmail.com>
2017-11-05 21:39:40 +0100

    travis: should fail

--- include/chord.file.h
+++ include/chord.file.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <errno.h>
 #include <sys/types.h>
 #include <attr/xattr.h>
 #include <experimental/filesystem>
@@ -11,6 +12,8 @@ namespace chord {
   struct attribute {
     bool success;
     std::string value;
+    attribute(bool succ):success(succ) {};
+    attribute(bool succ, std::string val):success(succ), value(val) {};
   };
 
   struct file {
@@ -40,22 +43,33 @@ namespace chord {
       return std::experimental::filesystem::create_directories({path});
     }
 
-    // xattr
+    /// xattr get
     static attribute attr(const std::string& path, const std::string& name) {
+      using namespace std::string_literals;
       size_t read = ::getxattr(path.c_str(), name.c_str(), nullptr, 0);
-      if(read == -1u) return {false, ""};
+      if(read == -1u) return {false};
 
       std::string value; value.resize(read);
 
       read = ::getxattr(path.c_str(), name.c_str(), value.data(), value.size());
-      if(read == -1u) throw new chord::exception("failed to read xattr");
+      if(read == -1u) throw new chord::exception("failed to get xattr"s + strerror(errno));
 
       return {true, value};
     }
 
+    /// xattr set
     static bool attr(const std::string& path, const std::string& name, const std::string& value) {
+      using namespace std::string_literals;
       size_t err = ::setxattr(path.c_str(), name.c_str(), value.data(), value.size(), 0);
-      if(err == -1u) throw new chord::exception("failed to read xattr");
+      if(err == -1u) throw new chord::exception("failed to set xattr"s + strerror(errno));
+
+      return true;
+    }
+
+    static bool attr_remove(const std::string& path, const std::string& name) {
+      using namespace std::string_literals;
+      size_t err = ::removexattr(path.c_str(), name.c_str());
+      if(err == -1u) throw new chord::exception("failed to remove xattr: "s + strerror(errno));
 
       return true;
     }
