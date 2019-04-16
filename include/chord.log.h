7f43c8b68aade582dc6f758c23be3b8ed4186f33
Christoph Winter <mail.christoph.winter@gmail.com>
2019-04-16 23:27:58 +0200

    * fix chord log wrapper/add singleton log factory draft

--- include/chord.log.h
+++ include/chord.log.h
@@ -1,14 +1,45 @@
 #pragma once
 
+#include "chord.file.h"
+#include "chord.singleton.h"
+
+#include <map>
+#include <vector>
+
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/bundled/ostream.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace chord {
 namespace log {
 
+enum class Category {
+  CHORD,
+  FILESYSTEM
+};
+
+class LoggerFactory final : public Singleton<LoggerFactory> {
+  public:
+    using sinks_vector_t = std::vector<spdlog::sink_ptr>;
+    using sinks_map_t = std::map<Category, sinks_vector_t>;
+  private:
+    sinks_map_t sinks;
+
+    void setup_chord();
+
+    void setup_filesystem();
+
+  public:
+    LoggerFactory();
+
+    std::shared_ptr<spdlog::logger> get_or_create(std::string name, Category category);
+};
+
 // NOTE this might be slow since get and create locks the registry
 //     with a mutex
-std::shared_ptr<spdlog::logger> get_or_create(std::string name);
+std::shared_ptr<spdlog::logger> get_or_create(std::string name, Category category=Category::CHORD);
+
 
 }  // namespace log
 }  // namespace chord
--- src/chord.log.cc
+++ src/chord.log.cc
@@ -1,16 +1,51 @@
 #include "chord.log.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 namespace chord {
 namespace log {
 
-// NOTE this might be slow since get and create locks the registry
-//     with a mutex
-std::shared_ptr<spdlog::logger> get_or_create(std::string name) {
-  auto logger = spdlog::get(name);
-  if (!logger) {
-    logger = spdlog::stdout_logger_mt(name);
-  }
-  return logger;
+void LoggerFactory::setup_chord() {
+      if(!chord::file::exists("logs")) {
+        chord::file::create_directory("logs");
+      }
+      const auto rotating_file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/chord.log", 1024*1024*20, 3);
+      rotating_file_sink->set_level(spdlog::level::trace);
+
+      sinks_vector_t chord_sinks;
+      chord_sinks.push_back(rotating_file_sink);
+      sinks.insert(make_pair(Category::CHORD, chord_sinks));
 }
+
+void LoggerFactory::setup_filesystem() {
+      const auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+      stdout_sink->set_level(spdlog::level::trace);
+
+      sinks_vector_t filesystem_sinks;
+      filesystem_sinks.push_back(stdout_sink);
+      sinks.insert(make_pair(Category::FILESYSTEM, filesystem_sinks));
+    }
+
+LoggerFactory::LoggerFactory() {
+      setup_chord();
+      setup_filesystem();
+}
+
+std::shared_ptr<spdlog::logger> LoggerFactory::get_or_create(std::string name, Category category) {
+      auto logger = spdlog::get(name);
+      if (!logger) {
+        const auto sinks = LoggerFactory::instance().sinks[category];
+        logger = std::make_shared<spdlog::logger>(name, begin(sinks), end(sinks));
+        logger->set_level(spdlog::level::trace);
+        spdlog::register_logger(logger);
+      }
+      return logger;
+    }
+
+std::shared_ptr<spdlog::logger> get_or_create(std::string name, chord::log::Category category) {
+  return LoggerFactory::instance().get_or_create(name, category);
+}
+
+
 }  // namespace log
 }  // namespace chord
