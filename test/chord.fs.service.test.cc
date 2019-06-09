a4fdd50074b9b0fd7a4933aabe16b8d1b8f864df
Christoph Winter <mail.christoph.winter@gmail.com>
2019-06-09 09:34:22 +0200

    * add service test for get by node_ref

--- test/chord.fs.service.test.cc
+++ test/chord.fs.service.test.cc
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#include <functional>
+
 #include <grpc++/server.h>
 #include <grpc++/server_builder.h>
 #include <grpc++/security/server_credentials.h>
@@ -26,6 +28,8 @@
 #include "chord.client.mock.h"
 #include "chord.service.mock.h"
 #include "chord.fs.metadata.manager.mock.h"
+#include "chord.fs.metadata.h"
+#include "chord.optional.h"
 
 using chord::test::TmpDir;
 using chord::test::TmpFile;
@@ -40,6 +44,8 @@ using grpc::ServerWriter;
 using grpc::Status;
 using grpc::InsecureServerCredentials;
 
+using chord::fs::Metadata;
+
 using chord::fs::PutRequest;
 using chord::fs::PutResponse;
 using chord::fs::GetRequest;
@@ -49,8 +55,11 @@ using ::testing::StrictMock;
 using ::testing::Eq;
 using ::testing::_;
 using ::testing::Return;
+using ::testing::Invoke;
+using ::testing::InvokeWithoutArgs;
 
 using namespace chord;
+using namespace chord::fs;
 using namespace chord::test::helper;
 
 class FsServiceTest : public ::testing::Test {
@@ -83,7 +92,7 @@ class FsServiceTest : public ::testing::Test {
     chord::MockClient* client;
     chord::MockService* service;
     //--- fs
-    chord::fs::IMetadataManager* metadata_mgr;
+    chord::fs::MockMetadataManager* metadata_mgr;
     std::unique_ptr<chord::ChordFacade> chord_facade;
 
 
@@ -102,9 +111,6 @@ TEST_F(FsServiceTest, get) {
   GetResponse res;
   ServerContext serverContext;
 
-  RouterEntry entry;
-  entry.set_endpoint("0.0.0.0:50050");
-  entry.set_uuid("0");
   EXPECT_CALL(*service, successor(_)).WillOnce(Return(make_entry("0", "0.0.0.0:50050")));
 
   TmpDir tmp;
@@ -116,3 +122,55 @@ TEST_F(FsServiceTest, get) {
   ASSERT_TRUE(chord::file::files_equal(source_file.path, target_file.path));
 }
 
+/**
+ * Called to get the file from the referenced node.
+ *
+ * Since we intentionally did not create the file for the first
+ * run, we need
+ */
+RouterEntry handle_get_from_node_reference(const chord::uri& source_uri, const chord::Context& context) {
+  const auto generated_file = context.data_directory / source_uri.path();
+
+  // deleted by TmpDir dtor
+  std::ofstream file(generated_file);
+  file << chord::uuid::random().string();
+  file.close();
+
+  return make_entry("0", "0.0.0.0:50050");
+}
+
+/**
+ * The first time the get is called the file does not exist but the
+ * metadata is referencing the same node (just for testing reasons).
+ * During the second call to self, the file is created within
+ * handle_get_from_node_reference().
+ */
+TEST_F(FsServiceTest, get_from_node_reference) {
+  GetRequest req;
+  GetResponse res;
+  ServerContext serverContext;
+
+  const auto source_uri = uri("chord:///file");
+  auto handler = std::bind(handle_get_from_node_reference, std::cref(source_uri), std::cref(context));
+
+  EXPECT_CALL(*service, successor(_))
+    .WillOnce(Return(make_entry("0", "0.0.0.0:50050")))  // initial
+    .WillOnce(InvokeWithoutArgs(handler)); // node ref - get create file to 'fake' accessing a different node
+
+  // reference ourself
+  Metadata metadata("file", "", "", perms::all, type::regular, chord::node{"0", "0.0.0.0:50050"});
+  EXPECT_CALL(*metadata_mgr, get(source_uri))
+    .WillRepeatedly(Return(std::set<Metadata>{metadata}));
+
+  // note without node reference for deletion
+  metadata = {"file", "", "", perms::all, type::regular};
+  EXPECT_CALL(*metadata_mgr, del(source_uri))
+    .WillOnce(Return(std::set<Metadata>{metadata}));
+
+  TmpDir tmp;
+  TmpFile target_file(tmp.path / "received_file");
+  const auto status = fs_client->get(source_uri, target_file.path);
+
+  ASSERT_TRUE(status.ok());
+  ASSERT_TRUE(chord::file::files_equal(context.data_directory / source_uri.path(), target_file.path));
+}
