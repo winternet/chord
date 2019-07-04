97372190b5e6c72844011e355477d91eb6690bf3
Christoph Winter <mail.christoph.winter@gmail.com>
2019-07-05 00:12:07 +0200

    * cleanup service.del test

--- test/chord.fs.service.del.test.cc
+++ test/chord.fs.service.del.test.cc
@@ -90,7 +90,7 @@ TEST_F(FilesystemServiceDelTest, del_file) {
     .WillOnce(Return(make_entry(self->context.node())))  // initial delete - flag (handle_del_dir)
     .WillOnce(Return(make_entry(self->context.node()))); // forward delete until not_found
 
-  // delete file
+  //--- delete file
   EXPECT_CALL(*self->metadata_mgr, exists(target_uri))
     .WillOnce(Return(true));
 
@@ -101,18 +101,14 @@ TEST_F(FilesystemServiceDelTest, del_file) {
   EXPECT_CALL(*self->metadata_mgr, del(target_uri))
     .WillOnce(Return(std::set<Metadata>{metadata}));
 
-  // delete folder
+  //--- delete folder
   Metadata metadata_dir(".", "", "", perms::none, type::directory, {}, {}, {});
   EXPECT_CALL(*self->metadata_mgr, get(root_uri))
     .WillOnce(Return(std::set<Metadata>{metadata_dir}))  // within del
     .WillOnce(Return(std::set<Metadata>{metadata_dir}))  // within handle_del_dir
-    .WillOnce(Return(std::set<Metadata>())); // forwarded delete -> not_found
+    .WillOnce(Return(std::set<Metadata>()));             // forwarded delete -> not_found
   EXPECT_CALL(*self->metadata_mgr, del(root_uri))
     .WillOnce(Return(std::set<Metadata>{metadata}));
-
-  // initial delete - flag
-  //EXPECT_CALL(*self->service, successor(_))
-  //  .WillOnce(Return(make_entry(self->context.node())));
 
   TmpFile target_file(self->context.data_directory / "file");
   ASSERT_TRUE(chord::file::exists(target_file.path));
@@ -124,123 +120,3 @@ TEST_F(FilesystemServiceDelTest, del_file) {
   ASSERT_FALSE(chord::file::exists(target_file.path));
   ASSERT_FALSE(chord::file::exists(self->context.data_directory));
 }
-
-//TEST_F(FilesystemServiceGetTest, get_from_node_reference) {
-//  TmpDir source_data_directory;
-//  const endpoint source_endpoint("0.0.0.0:50051");
-//  MockPeer source_peer(source_endpoint, source_data_directory);
-//
-//  const auto source_uri = uri("chord:///file");
-//  const auto source_file = source_data_directory.add_file("file");
-//  const auto source_file_backup = source_data_directory.path / "file.backup";
-//  file::copy_file(source_file.path, source_file_backup);
-//
-//  // connect the two nodes, first successor call will return self
-//  EXPECT_CALL(*self->service, successor(_))
-//    .WillOnce(Return(make_entry(self->context.node())))
-//    .WillRepeatedly(Return(make_entry(source_peer.context.node())));
-//  EXPECT_CALL(*source_peer.service, successor(_))
-//    .WillRepeatedly(Return(make_entry(self->context.node())));
-//
-//  // reference source peer
-//  Metadata metadata("file", "", "", perms::all, type::regular, {}, source_peer.context.node());
-//  EXPECT_CALL(*self->metadata_mgr, get(source_uri))
-//    .WillOnce(Return(std::set<Metadata>{metadata}));
-//
-//  // delete the node_ref and update the file hash
-//  metadata = {"file", "", "", perms::all, type::regular, crypto::sha256(source_file.path), {}};
-//  // update node_ref - file has been downloaded
-//  std::set<Metadata> metadata_set{metadata};
-//  EXPECT_CALL(*self->metadata_mgr, add(source_uri, metadata_set))
-//    .WillOnce(Return(true));
-//
-//  // no metadata exists on the node referenced by node_ref
-//  EXPECT_CALL(*source_peer.metadata_mgr, exists(source_uri))
-//    .WillOnce(Return(false));
-//
-//  TmpDir target_directory;
-//  const auto target_file = target_directory.path / "received_file";
-//  const auto status = self->fs_client->get(source_uri, target_file);
-//
-//  ASSERT_TRUE(status.ok());
-//  ASSERT_TRUE(chord::file::file_size(target_file) > 0);
-//  ASSERT_TRUE(chord::file::files_equal(source_file_backup, target_file));
-//
-//  // original file has been removed
-//  ASSERT_FALSE(chord::file::exists(source_file.path));
-//}
-//
-//TEST_F(FilesystemServiceGetTest, get_from_replication) {
-//  TmpDir source_data_directory;
-//  const endpoint source_endpoint("0.0.0.0:50051");
-//  MockPeer source_peer(source_endpoint, source_data_directory);
-//
-//  const auto source_uri = uri("chord:///file");
-//  const auto source_file = source_data_directory.add_file("file");
-//
-//  // connect the two nodes, first successor call will return self
-//  EXPECT_CALL(*self->service, successor(_))
-//    .WillOnce(Return(make_entry(self->context.node())))
-//    .WillRepeatedly(Return(make_entry(source_peer.context.node())));
-//  EXPECT_CALL(*source_peer.service, successor(_))
-//    .WillRepeatedly(Return(make_entry(self->context.node())));
-//
-//  // reference source peer
-//  Metadata metadata("file", "", "", perms::all, type::regular, {}, {}, Replication(2));
-//  EXPECT_CALL(*self->metadata_mgr, get(source_uri))
-//    .WillOnce(Return(std::set<Metadata>{metadata}));
-//
-//  TmpDir target_directory;
-//  const auto target_file = target_directory.path / "received_file";
-//  const auto status = self->fs_client->get(source_uri, target_file);
-//
-//  ASSERT_TRUE(status.ok());
-//  ASSERT_TRUE(chord::file::file_size(target_file) > 0);
-//  ASSERT_TRUE(chord::file::files_equal(source_file.path, target_file));
-//  // copied file to local data_directory
-//  ASSERT_TRUE(chord::file::files_equal(source_file.path, self->context.data_directory / "file"));
-//}
-//
-//TEST_F(FilesystemServiceGetTest, get_from_replication_propagates) {
-//  // peer holding the last replication
-//  TmpDir source_data_directory;
-//  const endpoint source_endpoint("0.0.0.0:50052");
-//  MockPeer source_peer(source_endpoint, source_data_directory);
-//
-//  // peer just to hop-over
-//  TmpDir middle_data_directory;
-//  const endpoint middle_endpoint("0.0.0.0:50051");
-//  MockPeer middle_peer(middle_endpoint, middle_data_directory);
-//
-//  const auto source_uri = uri("chord:///file");
-//  const auto source_file = source_data_directory.add_file("file");
-//
-//  // connect the two nodes, first successor call will return self
-//  EXPECT_CALL(*self->service, successor(_))
-//    .WillOnce(Return(make_entry(self->context.node())))
-//    .WillRepeatedly(Return(make_entry(middle_peer.context.node())));
-//  EXPECT_CALL(*middle_peer.service, successor(_))
-//    .WillRepeatedly(Return(make_entry(source_peer.context.node())));
-//  EXPECT_CALL(*source_peer.service, successor(_))
-//    .WillRepeatedly(Return(make_entry(self->context.node())));
-//
-//  // self peer metadata
-//  Metadata metadata("file", "", "", perms::all, type::regular, {}, {}, Replication(0,2));
-//  EXPECT_CALL(*self->metadata_mgr, get(source_uri))
-//    .WillOnce(Return(std::set<Metadata>{metadata}));
-//
-//  metadata.replication = {1,2};
-//  EXPECT_CALL(*middle_peer.metadata_mgr, get(source_uri))
-//    .WillOnce(Return(std::set<Metadata>{metadata}));
-//
-//  TmpDir target_directory;
-//  const auto target_file = target_directory.path / "received_file";
-//  const auto status = self->fs_client->get(source_uri, target_file);
-//
-//  ASSERT_TRUE(status.ok());
-//  ASSERT_TRUE(chord::file::file_size(target_file) > 0);
-//  ASSERT_TRUE(chord::file::files_equal(source_file.path, target_file));
-//  // copied file to local data_directories (middle/self)
-//  ASSERT_TRUE(chord::file::files_equal(source_file.path, self->context.data_directory / "file"));
-//  ASSERT_TRUE(chord::file::files_equal(source_file.path, middle_peer.context.data_directory / "file"));
-//}
