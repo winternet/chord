f4a6fe8856ca3a3819375a0046415bf599893e80
Christoph Winter <mail.christoph.winter@gmail.com>
2020-04-30 21:42:23 +0200

    add prefix path for ubuntu (?)

--- cmake/FindFsWatch.cmake
+++ cmake/FindFsWatch.cmake
@@ -2,24 +2,24 @@
 #
 # FsWatch_FOUND, if false, do not try to link to fswatch
 # FsWatch_LIBRARY, where to find fswatch
-# FsWatch_INCLUDE, where to find fswatch headers
+# FsWatch_INCLUDE_DIR, where to find fswatch headers
 #
+
 # Look for the header file.
-find_path(FsWatch_INCLUDE libfswatch/c++/monitor_factory.hpp
-    /usr/local/include/
-    /usr/include/
+find_path(
+    FsWatch_INCLUDE_DIR 
+    NAMES libfswatch/c++/monitor.hpp
     DOC "Path in which the file monitor_factory.h is located.")
 
 # find the FsWatch library
+set(CMAKE_PREFIX_PATH "/usr/lib/x86_64-linux-gnu")
 find_library(FsWatch_LIBRARY
     NAMES fswatch)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FsWatch DEFAULT_MSG FsWatch_INCLUDE FsWatch_LIBRARY)
+find_package_handle_standard_args(FsWatch DEFAULT_MSG FsWatch_INCLUDE_DIR FsWatch_LIBRARY)
 
 if(FsWatch_FOUND)
-  message(STATUS "Found fswatch (include: ${FsWatch_INCLUDE}, library: ${FsWatch_LIBRARY})")
-  set(FsWatch_INCLUDE ${FsWatch_INCLUDE})
-  set(FsWatch_LIBRARY ${FsWatch_LIBRARY})
-  mark_as_advanced(FsWatch_INCLUDE FsWatch_LIBRARY)
+  message(STATUS "Found fswatch (include: ${FsWatch_INCLUDE_DIR}, library: ${FsWatch_LIBRARY})")
+  mark_as_advanced(FsWatch_INCLUDE_DIR FsWatch_LIBRARY)
 endif()
