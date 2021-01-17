51e82cc28cec1770e49cd1bec3ecb21b125d2548
Christoph Winter <mail.christoph.winter@gmail.com>
2021-01-18 00:09:46 +0100

    add gtest dependency

--- cmake/integration_tests.cmake
+++ cmake/integration_tests.cmake
@@ -2,6 +2,8 @@ file(GLOB_RECURSE INTEGRATION_TEST_SOURCES test/integration/*.cc)
 file(GLOB TEST_UTIL_SOURCES test/util/*.cc)
 set(INTEGRATION_TEST_SOURCES ${INTEGRATION_TEST_SOURCES} ${TEST_UTIL_SOURCES})
 
+conan_basic_setup()
+
 string(REPLACE ";" "\n--   " INTEGRATION_TEST_SOURCES_OUT "${INTEGRATION_TEST_SOURCES}")
 message(STATUS "Found integration tests:\n--   ${INTEGRATION_TEST_SOURCES_OUT}")
 #
@@ -13,6 +15,6 @@ include_directories(SYSTEM "${CMAKE_CURRENT_BINARY_DIR}/gen")
 add_executable(${TEST_TARGET} ${INTEGRATION_TEST_SOURCES})
 target_link_libraries(${TEST_TARGET} 
   debug asan
-  general gtest gmock gmock_main ${PROJECT_NAME}++ ${signals_LIBRARY})
+  general ${CONAN_LIBS} ${PROJECT_NAME}++ ${signals_LIBRARY})
 target_link_libraries(${TEST_TARGET} --coverage)
 add_test(NAME ${TEST_TARGET} COMMAND chord_integration_test --gtest_output=xml:gtest-out/chord_integration_test.xml)
