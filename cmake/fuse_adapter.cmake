1ab794d33f12749288726dff835c8005ee33e2b8
Christoph Winter <mail.christoph.winter@gmail.com>
2022-03-10 22:48:33 +0100

    switch to conan find package multi generator

--- cmake/fuse_adapter.cmake
+++ cmake/fuse_adapter.cmake
@@ -3,11 +3,11 @@
 #
 set(FUSE_TARGET ${PROJECT_NAME}_fuse)
 
-include(FindPkgConfig)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
+conan_basic_setup(TARGETS)
 
-pkg_check_modules(FUSE REQUIRED fuse3)
+find_package(fusepp CONFIG REQUIRED)
+#pkg_check_modules(FUSE REQUIRED fuse3)
 if(FUSE_FOUND)
   message(STATUS "Found fuse (include: ${FUSE_INCLUDE_DIRS}, library: ${FUSE_LIBRARIES})")
 endif()
@@ -24,7 +24,7 @@ install(
 
 target_link_libraries(${FUSE_TARGET}
   debug asan
-  general ${PROJECT_NAME}++ ${signals_LIBRARY} ${CONAN_LIBS} ${FUSE_LIBRARIES})
+  general ${PROJECT_NAME}++ ${signals_LIBRARY} ${CONAN_LIBS} fusepp::fusepp)
 target_include_directories(${FUSE_TARGET} PRIVATE ${FUSE_INCLUDE_DIRS})
 set_target_properties(${FUSE_TARGET} PROPERTIES COMPILE_FLAGS "-D_FILE_OFFSET_BITS=64")
 
